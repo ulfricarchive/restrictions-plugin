c064dc8edfdc519040bdf2c0f8ab97eac043cc2a
Adam Harrison <ad@mharrison.us>
2017-12-05 14:46:24 -0500

    updates for staging

--- src/main/java/com/ulfric/plugin/restrictions/command/RestrictionStage.java
+++ src/main/java/com/ulfric/plugin/restrictions/command/RestrictionStage.java
@@ -4,8 +4,9 @@ import org.bukkit.event.EventHandler;
 
 import com.ulfric.dragoon.extension.inject.Inject;
 import com.ulfric.dragoon.stereotype.Stereotypes;
+import com.ulfric.plugin.commands.Command;
 import com.ulfric.plugin.commands.CommandPreRunEvent;
-import com.ulfric.plugin.commands.Stage;
+import com.ulfric.plugin.commands.stage.Stage;
 import com.ulfric.plugin.restrictions.RestrictedActionService;
 import com.ulfric.plugin.restrictions.RestrictedContext;
 
@@ -16,15 +17,7 @@ public class RestrictionStage extends Stage<String> {
 
 	@EventHandler
 	public void on(CommandPreRunEvent event) {
-		String restriction = commandToContext.computeIfAbsent(event.getCommandType(), command -> {
-			Restricted restricted = Stereotypes.getFirst(command, Restricted.class);
-
-			if (restricted == null) {
-				return null;
-			}
-
-			return restricted.value().isEmpty() ? null : restricted.value();
-		});
+		String restriction = get(event.getCommandType());
 
 		if (restriction != null) {
 			RestrictedContext context = new RestrictedContext();
@@ -35,6 +28,17 @@ public class RestrictionStage extends Stage<String> {
 				event.cancel(new RestrictionNotAllowedException(event.getContext(), restriction));
 			}
 		}
+	}
+
+	@Override
+	protected String compute(Class<? extends Command> command) {
+		Restricted restricted = Stereotypes.getFirst(command, Restricted.class);
+
+		if (restricted == null) {
+			return null;
+		}
+
+		return restricted.value().isEmpty() ? null : restricted.value();
 	}
 
 }
